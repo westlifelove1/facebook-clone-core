669230eb76cf13825fcf36e31b216039b63eb828
sam <thanhsonov@gmail.com>
2025-06-27 10:11:12 +0700

    update

---
+++ src/enums/friendstatus.enum.ts
@@ -0,0 +1,6 @@
+export enum FriendStatus {
+  FRIEND = 0,//already friends
+  PENDING_SENT = 1, //friend request sent by user
+  PENDING_RECEIVED = 2,//friend request received by user
+  NONE = 3,//not friends
+}
