17fb864d5e1b06e01c083b177b995e2dff1784f9
westlifelove1 <huynhtanvinhvl@gmail.com>
2025-06-30 14:16:06 +0700

    fix

--- src/modules/backend/postsaved/postsaved.service.ts
+++ src/modules/backend/postsaved/postsaved.service.ts
@@ -61,7 +61,7 @@ constructor(
   }
 
   findAll(userId : number) {
-    return this.postSavedRepository.find({where: { userId: userId }, relations: ['post', 'user'] ,
+    return this.postSavedRepository.find({where: { userId: userId }, relations: ['post', 'user', 'post.comments', 'post.reactions'] ,
     order: {
       updatedAt: 'DESC', 
     },});
